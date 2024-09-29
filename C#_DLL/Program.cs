04562cefd2afb15dc8e674d15623b7b6134bd1ef
jianghupt <160617542+jianghupt@users.noreply.github.com>
2024-09-29 11:17:17 +0800

    Add files via upload

---
+++ C#_DLL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace ABC
+{
+    internal class Program
+    {
+        static void GHI()
+        {
+            Console.WriteLine("Call GHI");
+        }
+        static void DEF()
+        {
+            Console.WriteLine("Call DEF");
+        }
+        static void ABC()
+        {
+            Console.WriteLine("Call ABC");
+        }
+        static void Main(string[] args)
+        {
+            ABC();
+        }
+    }
+}
